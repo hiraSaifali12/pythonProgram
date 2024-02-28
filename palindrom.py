40dabfc0f39f1bffe83a7611868a4cb143449f11
hiraSaifali12 <88941477+hiraSaifali12@users.noreply.github.com>
2024-02-28 18:01:52 +0530

    Create palindrom.py

---
+++ palindrom.py
@@ -0,0 +1,41 @@
+
+def pal(s):
+    n=len(s)
+    first,last=0,n-1
+    while(first<=last):
+        if s[first]==s[last]:
+            first+=1
+            last-=1
+        else:
+            return False
+    return True
+    
+def pal2(s):
+    n=len(s)
+    temp=s[::-1]
+    for i in range(n):
+        if temp==s:
+            return True
+        else:
+            return False
+
+            
+def pal3(s):
+    n=len(s)
+    for i in range(n):
+        if s[i]==s[n-i-1]:
+            return True 
+        else:
+            return False
+            
+def pal4(s):
+    temp=''.join(reversed(s))
+    if s==temp:
+        return True
+    else:
+        return False
+s="nitin"
+print(pal(s))
+print(pal12(s))        
+print(pal3(s))
+print(pal4(s))
